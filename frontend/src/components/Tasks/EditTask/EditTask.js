e12b44ecab3c26ba9f6699cd208f1d6aacacc29c
test20177 <34021403+test20177@users.noreply.github.com>
2021-11-17 16:31:05 +0100

    added edit task component

---
+++ frontend/src/components/Tasks/EditTask/EditTask.js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react"
+
+const EditTask = ({ task, saveTask, closeEditModal }) => {
+
+    const [title, setTitle] = useState(task.title)
+    const [body, setBody] = useState(task.body)
+    const [done, setDone] = useState(task.done)
+    
+    const [formErrors, setFormErrors] = useState({ title: ''})
+
+    const changeTitleHandler = event => setTitle(event.target.value)
+    const changeBodyHandler = event => setBody(event.target.value)
+    const changeStatusHandler = event => setDone(event.target.checked)
+    
+    const submitForm = (e) => {
+        e.preventDefault()
+        saveTask({
+            id: task.id, 
+            title, 
+            body, 
+            done
+        })
+        closeEditModal()
+    }  
+
+    useEffect(() => {
+        if (title <= 1) {
+            setFormErrors({...formErrors, title: 'Uzupełnij tytuł zadania'})
+        } else {
+            setFormErrors({...formErrors, title: ''}) 
+        }
+    }, [title])
+
+    const isFormErrors = Object.values(formErrors).filter(x => x).length;
+
+    return (
+        <>
+            <h4 className="text-center">Edycja zlecenia</h4>
+            <form onSubmit={submitForm} className="mb-3">
+                <div className="row">
+                    <label htmlFor="title" className="col-sm-2 col-form-label">Tytuł</label>
+                    <div className="col-sm-10">
+                        <input 
+                            type="text" 
+                            id="title" 
+                            className={`form-control && ${formErrors.title && 'is-invalid'}`}
+                            value={title} 
+                            onChange={changeTitleHandler} >
+                        </input>
+                        <div className="invalid-feedback">
+                            {formErrors.title}
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-2 mb-1">
+                    <label htmlFor="body" className="col-sm-2 col-form-label">Treść</label>
+                    <div className="col-sm-10">
+                        <textarea 
+                            id="body" 
+                            className="form-control" 
+                            value={body} 
+                            onChange={changeBodyHandler}
+                            rows="3">
+                        </textarea>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-2">Zrealizowane</div>
+                    <div className="col-sm-10">
+                        <div className="form-check">
+                            <input 
+                                type="checkbox" 
+                                id="done"
+                                className="form-check-input" 
+                                checked={done}
+                                onChange={changeStatusHandler} >
+                            </input>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-primary btn-block m-2"
+                            onClick={closeEditModal}>
+                            Anuluj
+                    </button>
+                    <button className="btn btn-primary btn-block m-2"
+                            disabled={isFormErrors}>
+                            Zapisz
+                    </button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default EditTask
