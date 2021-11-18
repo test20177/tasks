e5658a108ebe332a32d7f8f87bd0e1cb77bfdf3e
test20177 <34021403+test20177@users.noreply.github.com>
2021-11-18 09:40:43 +0100

    before axios

--- frontend/src/components/Tasks/Tasks.js
+++ frontend/src/components/Tasks/Tasks.js
@@ -1,9 +1,9 @@
 import Task from './Task/Task'
-import './Tasks.css'
 import Modal from 'react-modal'
-
+import { deleteModalStyle, editModalStyle } from './Tasks.css.js'
 import { useState, useEffect } from 'react'
 import DeleteTask from './DeleteTask/DeleteTask'
+import EditTask from './EditTask/EditTask'
 import NewTask from './NewTask/NewTask'
 
 let initTasks = [
@@ -19,131 +19,151 @@ let initTasks = [
     body: 'Wymienić bezpiecznik w rozdzielni',
     done: true
   },
-] 
+]
 
 const Tasks = () => {
 
   const [tasks, setTasks] = useState(initTasks)
   const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [showEditTaskModal, setShowEditTaskModal] = useState(false)
   const [showNewTaskForm, setNewTaskForm] = useState(false)
   const [currentTask, setCurrentTask] = useState()
 
-  useEffect( () => {
-      Modal.setAppElement('body')
+  useEffect(() => {
+    Modal.setAppElement('body')
   }, [])
-    
-  useEffect( () => {
-    const uncompletedTasks = tasks.filter( task => task.done === false ).length
-    document.title = 'Zlecenia '
-      + ( uncompletedTasks 
-          ? ` (niezrealizowanych ${uncompletedTasks})` 
-          : ''
-      )
-  }, [tasks])  
 
-  const closeDeleteModal = () => setShowDeleteModal(false)
-  
+  useEffect(() => {
+    const uncompletedTasks = tasks.filter(task => task.done === false).length
+    document.title = 'Zlecenia '
+      + (uncompletedTasks
+        ? ` (niezrealizowanych ${uncompletedTasks})`
+        : ''
+      )
+  }, [tasks])
+
   const openDeleteModal = () => setShowDeleteModal(true)
+  const closeDeleteModal = () => setShowDeleteModal(false)
+
+  const openEditModal = () => setShowEditTaskModal(true)
+  const closeEditModal = () => setShowEditTaskModal(false)
 
   const changeTaskStatus = (id) => {
-      let newTasks = [...tasks]
-      newTasks.find( task => task.id === id).done = !newTasks.find( task => task.id === id).done
-      setTasks(newTasks)
+    let newTasks = [...tasks]
+    newTasks.find(task => task.id === id).done = !newTasks.find(task => task.id === id).done
+    setTasks(newTasks)
   }
 
-  const deleteTask = (id) => {
-    setTasks( tasks.filter( task => task.id !== id ) )
-  }
-
-  const addTask = (task => {
+  const addTask = task => {
     const newTasks = [...tasks]
     newTasks.push(task)
     setTasks(newTasks)
     setNewTaskForm(false)
-  })
+  }
+
+  const saveTask = task => {
+    const newTasks = [...tasks]
+    const index = newTasks.findIndex(item => item.id === task.id)
+    if (index >= 0) {
+      newTasks[index] = task
+      setTasks(newTasks)
+    }
+  }
+
+  const deleteTask = id => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
 
   return (
     <>
       <Modal
         isOpen={showDeleteModal}
         contentLabel="Potwierdzenie usunięcia"
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-          }
-        }}
+        style={deleteModalStyle}
       >
-        <DeleteTask 
-          task={currentTask} 
+        <DeleteTask
+          task={currentTask}
           deleteTask={deleteTask}
           closeDeleteModal={closeDeleteModal}
         />
       </Modal>
-      
-      { !showNewTaskForm ? (
+
+      <Modal
+        isOpen={showEditTaskModal}
+        contentLabel="Edycja zlecenia"
+        style={editModalStyle}
+      >
+        <EditTask
+          task={currentTask}
+          saveTask={(task) => saveTask(task)}
+          closeEditModal={closeEditModal}
+        />
+      </Modal>
+
+
+      {!showNewTaskForm ? (
         <div className="row mt-4">
           <div className="col-2">
             <h4>Lista zleceń</h4>
           </div>
           <div className="col-10">
-            <button type="button" 
-                    className="btn btn-sm btn-primary" 
-                    onClick={() => setNewTaskForm(true)}>
-                    Dodaj zlecenie
+            <button type="button"
+              className="btn btn-sm btn-primary"
+              onClick={() => setNewTaskForm(true)}>
+              Dodaj zlecenie
             </button>
           </div>
         </div>
       ) : (
         <div className="row mt-4">
           <div className="col">
-            <NewTask 
-                onAdd={(task) => addTask(task)}
-                onCancel={() => setNewTaskForm(false)}
+            <NewTask
+              onAdd={(task) => addTask(task)}
+              onCancel={() => setNewTaskForm(false)}
             />
           </div>
         </div>
       )}
-      
+
       <div className="row mt-2">
-          <div className="col-12">
-          { 
-              tasks?.length ? (
-                  <table className="table table-striped text-center">
-                      <caption>
-                          <h5>Suma: {tasks.length}</h5>
-                      </caption>
-                      <thead>
-                          <tr>
-                              <th scope="col">#</th>
-                              <th scope="col">Tytuł</th>
-                              <th scope="col">Treść</th>
-                              <th scope="col">Status</th>
-                              <th scope="col">Edycja</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          { tasks.map( (task, index) => (
-                              <Task 
-                                key={index} 
-                                task={task} 
-                                onDelete={() => {     
-                                  setCurrentTask(task)
-                                  openDeleteModal()
-                                }}
-                                onChangeStatus={changeTaskStatus} />
-                          )) }    
-                      </tbody>
-                      </table>
-              ) : (
-                  <h3 className="text-secondary" >Brak zleceń</h3>
-              )
+        <div className="col-12">
+          {
+            tasks?.length ? (
+              <table className="table table-striped text-center">
+                <caption>
+                  <h5>Suma: {tasks.length}</h5>
+                </caption>
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Tytuł</th>
+                    <th scope="col">Treść</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Edycja</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tasks.map((task, index) => (
+                    <Task
+                      key={index}
+                      task={task}
+                      onDelete={() => {
+                        setCurrentTask(task)
+                        openDeleteModal()
+                      }}
+                      onEdit={() => {
+                        setCurrentTask(task)
+                        openEditModal()
+                      }}
+                      onChangeStatus={changeTaskStatus} />
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <h3 className="text-secondary" >Brak zleceń</h3>
+            )
           }
-          </div>
+        </div>
       </div>
     </>
   )
